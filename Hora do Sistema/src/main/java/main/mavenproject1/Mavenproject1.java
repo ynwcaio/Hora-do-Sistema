1791b3f47099c1ae5010ed227c4ddba38b9187bc
Caio Gonçalves Pacheco <165097399+ynwcaio@users.noreply.github.com>
2024-08-14 19:19:06 -0300

    Add files via upload

---
+++ Hora do Sistema/src/main/java/main/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+
+
+package main.mavenproject1;
+
+import java.util.Date;
+
+
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é :");
+        System.out.println(relogio.toString());
+        
+    }
+}
